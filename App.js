934fd6ffa7e49ead049ac53c8dfd4dcbb4672539
anaesperanzalin <anaesperanzalin@users.noreply.github.com>
2021-02-17 11:23:19 -0800

    icon problems

--- App.js
+++ App.js
@@ -1,48 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import DeckList from './components/DeckList'
-import { TabNavigator} from '@react-navigation/native'
 import { FontAwesome, MaterialCommunityIcons } from '@expo/vector-icons' 
 import { blue, white } from './utils/colors'
 import AddDeck from './components/AddDeck'
-
-
-const Tab = TabNavigator({
-  DeckList: {
-    screen: DeckList,
-    navigationOptions: {
-      tabBarLabel: 'Deck List',
-      tabBarIcon: ({tintColor}) => <MaterialCommunityIcons name= 'cards' size= {30} color = {tintColor}/>
-
-
-    }
-  }, 
-
-  AddDeck : {
-    screen: AddDeck,
-    navigationOptions: {
-      tabBarLabel: 'Add Deck',
-      tabBarIcon: ({tintColor}) =><FontAwesome name='plus-square' size={30} color={tintColor}/>
-    }
-
-  }
-
-
-}
-
-)
+import TabNavigator from './components/TabNavigator'
+import { NavigationContainer } from '@react-navigation/native';
 
 
 
 export default function App() {
   return (
-    <View style={{flex :1 }}>
-
-    <Tab/>
-     
+    <NavigationContainer>
+    <View style={{flex : 1}}>
+      
+      <TabNavigator/>
       
     </View>
+    </NavigationContainer>
   );
 }
 
